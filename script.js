0ac47dab34f66f39dd491e064e72f62fd5091baf
Yan-Carvalho <76734082+Yan-Carvalho@users.noreply.github.com>
2021-01-12 18:50:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+$(function(){
+
+    $(".carousel").slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows:false
+       
+    })
+})
